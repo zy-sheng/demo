6cc524c58b6b0f228a65ed8ffe3d7ddbfc817dda
= <2690515220@qq.com>
2020-03-05 15:16:00 +0800

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class demo2 {
+public static void main(String[] args) {
+	System.out.println("1");
+	System.out.println("Hello world");
+	System.out.println("3");
+	System.out.println("2");
+	
+}
+}
